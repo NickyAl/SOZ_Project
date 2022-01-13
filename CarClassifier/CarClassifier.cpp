fee07f4ad89a156a436c0de54c95c28419f79302
Nikolay Aleksiev <nikitotp@gmail.com>
2022-01-13 20:07:40 +0200

    cleaned up code and added final csv files

--- CarClassifier/CarClassifier.cpp
+++ CarClassifier/CarClassifier.cpp
@@ -10,7 +10,107 @@
 double PERCENTAGE_FOR_TRAINING = 0.7; //by testing we decided that 80% and k=5 is the best combination and it is used when we input custom patients
 size_t K = 3;
 
-static void trainTestSplit(std::vector<std::vector<double>>& X, std::vector<std::string>& y,
+//already a number
+const size_t MODEL_POSITION = 0;
+const size_t YEAR_POSITION = 1;
+const size_t PRICE_POSITION = 2;
+const size_t MILEAGE_POSITION = 4;
+const size_t TAX_POSITION = 6;
+const size_t MPG_POSITION = 7;
+const size_t ENGINE_SIZE_POSITION = 8;
+
+//needs to be processed to a number
+const size_t TRANSMISSION_POSITION = 3;
+
+//functions
+//splits data into parts for training and parts for testing
+void trainTestSplit(std::vector<std::vector<double>>& X, std::vector<std::string>& y,
+    std::vector<std::vector<double>>& XTrain, std::vector<std::vector<double>>& XTest,
+    std::vector<std::string>& yTrain, std::vector<std::string>& yTest);
+
+//normalizes columns (divides each element of the vector by the lenght of the vector this way the new vector has length = 1)
+void normalize(std::vector<std::vector<double>>& values, size_t col);
+
+//collects the raw data from the file
+std::vector<std::vector<std::string>> readFromFile(std::ifstream& file);
+
+//separates the classes names from the raw data
+std::vector<std::string> getCLasses(std::vector<std::vector<std::string>> rawData);
+
+//pre-processes the raw data so it can be used for kNN
+std::vector<std::vector<double>> preProcessData(std::vector<std::vector<std::string>> rawData);
+
+int main()
+{
+    auto frame_start = std::chrono::high_resolution_clock::now(); //starts a timer
+
+    //open csv file containing the data set
+    std::ifstream dataSetFile("hyundi.csv");
+
+    if (!dataSetFile.is_open())
+    {
+        std::cerr << "File did not open.\n";
+        return 0;
+    }
+
+    //get the data and pre-process it
+    std::vector<std::vector<std::string>> rawData = readFromFile(dataSetFile);
+    dataSetFile.close(); //close the file as we no longer need it
+
+    std::vector<std::string> classes = getCLasses(rawData);
+
+    std::vector<std::vector<double>> data = preProcessData(rawData);
+
+    //normalize the data
+    const size_t PROCESSED_YEAR_POSITION = YEAR_POSITION - 1;
+    const size_t PROCESSED_PRICE_POSITION = PRICE_POSITION - 1;
+    const size_t PROCESSED_MILEAGE_POSITION = MILEAGE_POSITION + 1;
+    const size_t PROCESSED_TAX_POSITION = TAX_POSITION + 4;
+    const size_t PROCESSED_MPG_POSITION = MPG_POSITION + 4;
+    const size_t PROCESSED_ENGINE_SIZE_POSITION = ENGINE_SIZE_POSITION + 4;
+
+    normalize(data, PROCESSED_YEAR_POSITION);
+    normalize(data, PROCESSED_PRICE_POSITION);
+    normalize(data, PROCESSED_MILEAGE_POSITION);
+    normalize(data, PROCESSED_TAX_POSITION);
+    normalize(data, PROCESSED_MPG_POSITION);
+    normalize(data, PROCESSED_ENGINE_SIZE_POSITION);
+
+    //split the data into parts for testing and parts for training
+    std::vector<std::vector<double>> XTrain;
+    std::vector<std::vector<double>> XTest;
+
+    std::vector<std::string> yTrain;
+    std::vector<std::string> yTest;
+
+    trainTestSplit(data, classes, XTrain, XTest, yTrain, yTest);
+
+    //start kNN
+    KNN model(K);
+    model.fit(XTrain, yTrain);
+    std::vector<std::string> yPred = model.predict(XTest);
+
+    size_t correct = 0;
+    size_t sizeYPred = yPred.size();
+   
+    for (size_t i = 0; i < sizeYPred; i++)
+    {
+        if (yPred[i] == yTest[i])
+        {
+            correct++;
+        }
+    }
+
+    std::cout << correct << " out of " << sizeYPred << " are correct\n";
+    std::cout << "Success rate: " << (double(correct) / double(sizeYPred)) * 100 << "%\n";
+
+    auto frame_end = std::chrono::high_resolution_clock::now(); //end of timer
+
+    std::chrono::duration<float> duration = frame_end - frame_start;
+    std::cout << "Run time:" << duration.count() << '\n';
+}
+
+void trainTestSplit(std::vector<std::vector<double>>& X, std::vector<std::string>& y,
     std::vector<std::vector<double>>& XTrain, std::vector<std::vector<double>>& XTest,
     std::vector<std::string>& yTrain, std::vector<std::string>& yTest)
 {
@@ -45,54 +145,27 @@ void normalize(std::vector<std::vector<double>>& values, size_t col)
 {
     double sum = 0;
     size_t size = values.size();
-    for (size_t i = 0; i < size; i += 10)
+    for (size_t i = 0; i < size; i++)
     {
         sum += pow(values[i][col], 2);
     }
 
     double magnitude = sqrt(sum);
 
-    for (size_t i = 0; i < size; i += 10)
+    for (size_t i = 0; i < size; i++)
     {
         values[i][col] /= magnitude;
     }
 }
 
-int main()
+std::vector<std::vector<std::string>> readFromFile(std::ifstream& file)
 {
-    auto frame_start = std::chrono::high_resolution_clock::now();
-
-    std::ifstream file("toyota.csv");
-
-    if (!file.is_open())
-    {
-        std::cerr << "File did not open.\n";
-        return 0;
-    }
-
-    //already a number
-    const size_t MODEL_POSITION = 0;
-    const size_t YEAR_POSITION = 1;
-    const size_t PRICE_POSITION = 2;
-    const size_t MILEAGE_POSITION = 4;
-    const size_t TAX_POSITION = 6;
-    const size_t MPG_POSITION = 7;
-    const size_t ENGINE_SIZE_POSITION = 8;
-
-    //needs to be processed to a number
-    const size_t TRANSMISSION_POSITION = 3;
-
-
-
-
     std::string str = "";
     std::vector<std::vector<std::string>> rawData; //the data we have before we process it
     std::vector<std::string> line; //used to separate each sample
-    std::vector<std::string> labels; //used to keep the name of each column
     bool isFirstLine = true;
     char symbol = '\0';
 
-    std::cout << "Started loading data\n";
     while (symbol != -1) //savess the data from the file to rawData
     {
         symbol = file.get();
@@ -101,14 +174,12 @@ int main()
         {
             if (symbol == ',')
             {
-                labels.push_back(str);
                 str = "";
                 continue;
             }
             else if (symbol == '\n')
             {
                 isFirstLine = false;
-                labels.push_back(str);
                 str = "";
                 continue;
             }
@@ -136,11 +207,29 @@ int main()
 
         str += symbol;
     }
-    std::cout << "Finished loading data\n";
 
+    return rawData;
+}
+
+std::vector<std::string> getCLasses(std::vector<std::vector<std::string>> rawData)
+{
+    std::vector<std::string> classes; //y or the model of the car
+
+    size_t rowsRawData;
+    rowsRawData = rawData.size();
+
+    for (size_t i = 0; i < rowsRawData; i++) //separates classes from the rest of the data
+    {
+        classes.push_back(rawData[i][MODEL_POSITION]);
+    }
+
+    return classes;
+}
+
+std::vector<std::vector<double>> preProcessData(std::vector<std::vector<std::string>> rawData)
+{
     std::vector<std::vector<double>> data; //the procesed data
     std::vector<double> sample;
-    std::vector<std::string> classes; //y or the types of drugs
 
     size_t rowsRawData, colsRawData;
     rowsRawData = rawData.size();
@@ -148,16 +237,8 @@ int main()
 
     double num = 0;
 
-    std::cout << "Loading classes\n";
-    for (size_t i = 0; i < rowsRawData; i += 10) //separates classes from the rest of the data
-    {
-        classes.push_back(rawData[i][MODEL_POSITION]);
-    }
-    std::cout << "Finished loading classes\n";
-
-    std::cout << "Processing data\n";
     double numberValue = 0;
-    for (size_t i = 0; i < rowsRawData; i += 10) //processes and saves all the samples in an array
+    for (size_t i = 0; i < rowsRawData; i++) //processes and saves all the samples in an array
     {
         for (size_t j = 1; j < colsRawData; j++)
         {
@@ -227,97 +308,6 @@ int main()
             sample.pop_back();
         }
     }
-    std::cout << "Finished processing data\n";
 
-    //Normalize data
-    const size_t PROCESSED_YEAR_POSITION = YEAR_POSITION - 1;
-    const size_t PROCESSED_PRICE_POSITION = PRICE_POSITION - 1;
-    const size_t PROCESSED_MILEAGE_POSITION = MILEAGE_POSITION + 1;
-    const size_t PROCESSED_TAX_POSITION = TAX_POSITION + 4;
-    const size_t PROCESSED_MPG_POSITION = MPG_POSITION + 4;
-    const size_t PROCESSED_ENGINE_SIZE_POSITION = ENGINE_SIZE_POSITION + 4;
-
-    //normalize(data, PROCESSED_YEAR_POSITION);
-    normalize(data, PROCESSED_PRICE_POSITION);  
-    normalize(data, PROCESSED_MILEAGE_POSITION);
-    normalize(data, PROCESSED_TAX_POSITION); //
-    //normalize(data, PROCESSED_MPG_POSITION);
-    //normalize(data, PROCESSED_ENGINE_SIZE_POSITION);
-    
-
-    size_t rowsData, colsData;
-    rowsData = data.size();
-    colsData = data[0].size();
-
-    ////year
-    double sum = 0;
-    size_t counter = 0;
-    for (size_t i = 0; i < rowsData; i += 10)
-    {
-        counter++;
-        sum += data[i][PROCESSED_YEAR_POSITION];
-    }
-
-    //double avg = sum / counter;
-
-    //for (size_t i = 0; i < rowsData; i += 10)
-    //{
-    //    data[i][PROCESSED_YEAR_POSITION] -= avg;
-    //    data[i][PROCESSED_YEAR_POSITION] /= 10;
-    //}
-
-    //tax
-    //std::cout << "TAX:" << data[0][PROCESSED_TAX_POSITION] << "\n";
-    //for (size_t i = 0; i < rowsData; i += 10)
-    //{
-    //    data[i][PROCESSED_TAX_POSITION] /= 50;
-    //}
-
-    //////mpg
-    std::cout << "MPG:" << data[0][PROCESSED_MPG_POSITION] << "\n";
-    for (size_t i = 0; i < rowsData; i += 10)
-    {
-        data[i][PROCESSED_MPG_POSITION] /= 50;
-    }
-
-    std::vector<std::vector<double>> XTrain;
-    std::vector<std::vector<double>> XTest;
-
-    std::vector<std::string> yTrain;
-    std::vector<std::string> yTest;
-
-    std::cout << "Started spliting data\n";
-    trainTestSplit(data, classes, XTrain, XTest, yTrain, yTest);
-    std::cout << "Finished spliting data\n";
-
-    KNN model(K);
-
-    std::cout << "Started fit\n";
-    model.fit(XTrain, yTrain);
-    std::cout << "Finisjed fit\n";
-    std::cout << "Started predicting\n";
-    std::vector<std::string> yPred = model.predict(XTest);
-
-    size_t correct = 0;
-    size_t size = yPred.size();
-
-   
-    for (size_t i = 0; i < size; i++)
-    {
-        if (yPred[i] == yTest[i])
-        {
-            correct++;
-        }
-    }
-
-    std::cout << correct << " out of " << size << " are correct\n";
-    std::cout << "Success rate: " << (double(correct) / double(size)) * 100 << "%\n";
-
-    file.close();
-
-
-    auto frame_end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<float> duration = frame_end - frame_start;
-    std::cout << duration.count() << '\n';
+    return data;
 }
